2ea20802bb9ca061b14d25473046c7e25faa2309
Swing-code <13299161882@163.com>
2022-02-04 16:58:53 +0800

    write done chapter02

--- chapter02/course/Vector/Vector.h
+++ chapter02/course/Vector/Vector.h
@@ -13,6 +13,12 @@ protected:
     void copyFrom(T const * A, Rank lo, Rank hi);
     void expand();
     void shrink();
+    static Rank binSearch(T * A, T const & e, Rank lo, Rank hi);//二分搜索
+    void swap(T & a, T & b);
+    bool bubble(Rank lo, Rank hi);  // 扫描交换
+    void bubbleSort(Rank lo, Rank hi);  // 冒泡排序
+    void merge(Rank lo, Rank mi, Rank hi);  // 归并算法
+    void mergeSort(Rank lo, Rank hi);   //
 public:
     Vector(int c = DEFAULT_CAPACITY) {_elem = new T[_capacity = c]; _size = 0;} // 默认
     Vector(T const * A, Rank lo, Rank hi) { copyFrom(A, lo, hi); }
@@ -43,6 +49,7 @@ public:
     int disordered() const;
 // 去重
     int uniquify();
+// 查找
 };
 
 
@@ -68,6 +75,99 @@ void Vector<T>::expand()    // 向量空间不足时扩容
         _elem[i] = oldElem[i];      // T为基本类型或已重载运算符"="
     delete []oldElem;   // 释放原空间
 }   // 得益于向量的封装，扩容之后数据区的物理地址有所改变，却不致出现野指针
+
+// 二分查找算法
+template <typename T>
+static Rank binSearch(T * A, T const & e, Rank lo, Rank hi)
+{
+    while (lo < hi)
+    { //每步迭代仅需一次判断，有两个分支
+        Rank mi = (lo + hi) >> 1;   //以中点为轴点
+        (e < A[mi]) ? hi = mi : lo = mi + 1;    //经比较后确定深入[lo,mi)或(mi,hi)
+    } //成功查找不能提前终止
+    return --lo; //循环结束时，lo为大于e元素的最小秩，故lo-1即不大于e的元素的最大秩
+} //有多个命中元素时，总能返回秩最大者；查找失败时，返回失败的位置
+
+// 交换算法
+template <typename T>
+void Vector<T>::swap(T & a, T & b)
+{
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+// 扫描交换算法
+template <typename T>
+bool Vector<T>::bubble(Rank lo, Rank hi)
+{ //一趟扫描交换
+    bool sorted = true; // 整体有序标志
+    while (++lo < hi)   // ++lo：自左向右逐一检查各相邻元素
+    {
+        if (_elem[lo] < _elem[lo - 1])
+        { // 若逆序
+            sorted = false; // 意味着尚未整体有序
+            swap(_elem[lo], _elem[lo - 1]);//交换使得局部有序
+        }
+    }
+    return sorted;  // 返回有序标志
+}
+
+// 起泡排序
+template <typename T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi)
+{
+    while (!bubble(lo,hi--));   // 逐趟扫描交换，直至全序
+    // while (lo < --hi)
+    //     bubble(lo, hi);  // 该算法没有利用返回的有序标志，最好情况和最坏情况一样
+}
+
+// 归并算法
+template <typename T>   // 有序向量的归并
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi)
+{ // 各自有序的子向量[lo,mi)和[mi,hi)
+    T * A = _elem + lo; // 合并后的向量A[0,ho-lo) = _elem[lo, hi)
+    int lb = mi - lo;
+    T * B = new T[lb]; // 前子向量B[0,lb) = _elem[lo,mi)
+    for (Rank i = 0; i < lb; B[i] = A[i++]) // 复制前子向量
+    int lc = hi - mi;
+    T * C = _elem + mi; // 后子向量C[0,lc) = _elem[mi,hi)
+    for (Rank i = 0, j = 0, k = 0;(j < lb) || (k < lc))
+    { //B[j]和C[k]中的小者续至A末尾
+        if ((j < lb) && (!(k < lc) || (B[j] <= C[k])))
+            A[i++] = B[j++];
+        if ((k < lc) && (!(j < lb) || (B[j] > C[k])))
+            A[i++] = C[k++];
+    }
+    delete [] B; // 释放临时空间B
+    // 归并后得到完整有序的向量[lo, hi)
+    // T * B = new T[mi - lo];
+    // for (Rank i = 0; i < mi - lo; i++)
+    //     B[i] = A[i];
+    // T * C = _elem + mi;
+    // for (Rank i = 0, j = 0, k = 0;(j < (mi - lo) || k < (hi - mi));)
+    // {
+    //     if ((j < (mi - lo)) && ((k >= (hi - mi)) || (B[j] <= C[k])))
+    //         A[i++] = B[j++];
+    //     if ((k < (hi - mi)) && ((j >= (mi - lo)) || (C[k] < B[j])))
+    //         A[i++] = C[k++];
+    // }
+    // delete [] B;
+}
+// 归并排序算法
+template <typename T> // 向量归并排序
+void Vector<T>::mergeSort(Rank lo, Rank hi)
+{ // 0 <= lo <= hi <= _size
+    if (lo - hi < 2) // 单元素区间自然有序，否则..
+        return;
+    Rank mi = (lo + hi) >> 1; // 以中点为界
+    mergeSort(lo,mi);
+    mergeSort(mi,hi); // 分别排序
+    merge(lo, mi, hi); // 归并
+}
+
+
 
 /************************* 无序向量 ******************************/
 // 插入算法
--- chapter02/course/fibSearch.cpp
+++ chapter02/course/fibSearch.cpp
@@ -8,10 +8,10 @@ static Rank fibSearch(T * S, T const & e, Rank lo, Rank hi)
     while (lo < hi)
     {
         while (hi - lo < fib.get()) fib.prev();
-        Rank mi = (lo + hi) >> 1;   // 轴点居中(区间宽度折半，等效于其数值右移一位)
+        Rank mi = lo + fib.get() - 1;   // 确定形如Fib(k) - 1的轴点
         if (e < S[mi]) hi = mi;     // 深入前半段[lo,mi)
         else if (S[mi] < e) lo = mi + 1;    // 深入后半段(mi,hi)
         else return mi;
-    }
-    return -1;
-}
+    } // 成功查找可以提前终止
+    return -1;  // 查找失败
+}   // 命中多个元素时，不能保证返回最大者；失败时，简单的返回-1而不能指示失败的位置
