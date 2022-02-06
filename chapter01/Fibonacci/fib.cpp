f3ac9b5f9149504a228af4f9d54c7e869bbef702
Swing-code <13299161882@163.com>
2022-02-06 13:42:37 +0800

    add line recrusion

--- chapter01/Fibonacci/fib.cpp
+++ chapter01/Fibonacci/fib.cpp
@@ -1,5 +1,8 @@
+// fib.cpp -- 计算第n个斐波那契数
 #include <iostream>
-long long fib_R(int n);
+#include <ctime>
+long long fib_R(int n); // 二分递归
+long long fib_LR(int n, long long & prev); // 线性递归
 // 算法导论 15.1-5
 long long fib_M(int n, long long M[]);         // 记忆递归+Memoization
 long long fib_D(int n);         // 动态规划
@@ -7,20 +10,49 @@ int main()
 {
     using namespace std;
     int n;
+    clock_t start, end; // 统计计算时间
+    long long result;   // 保存计算结果
+    long long prev;     // 保存斐波那契数列前一项
     cout << "Enter a integer: ";
     cin >> n;
-    long long * M = new long long[n + 1];
-    for (int i = 0; i < n; i++)
-        cout << i << ": " << fib_D(i) << flush << "\t"
-             << fib_M(i, M) << flush<< "\t"
-             << fib_R(i) << endl;
+    // 迭代版
+    start = clock();
+    result = fib_D(n);
+    end = clock();
+    cout << "Iterator version: " << result << " time: "
+         << double(end - start)/CLOCKS_PER_SEC << " secs" << endl;
+    // 线性递归版
+    start = clock();
+    result = fib_LR(n, prev);
+    end = clock();
+    cout << "Line Recursion version: " << result << " time: "
+         << double(end - start)/CLOCKS_PER_SEC << " secs" << endl;
+    // 二分递归版
+    start = clock();
+    result = fib_R(n);
+    end = clock();
+    cout << "Recursion version: " << result << " time: "
+         << double(end - start)/CLOCKS_PER_SEC << " secs" << endl;
     return 0;
 }
 long long fib_R(int n)
 {
     return n < 2 ? n : fib_R(n - 1) + fib_R(n - 2);
 }
-
+long long fib_LR(int n, long long & prev)
+{
+    if (0 == n)
+    {
+        prev = 1;
+        return 0;
+    }
+    else
+    {
+        long long preprev;
+        prev = fib_LR(n - 1, preprev) ;
+        return preprev + prev;
+    }
+}
 long long fib_M(int n, long long M[])
 {
     if (n < 2)
