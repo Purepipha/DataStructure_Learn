edc612ee31e4586e1f1e98cf52f79810cc0a5b7d
Swing-code <13299161882@163.com>
2022-02-17 15:53:36 +0800

    improve by use swap to instead of move

--- code/List/list/list_selectionSort.h
+++ code/List/list/list_selectionSort.h
@@ -5,10 +5,14 @@ void List<T>::selectionSort(Posi(T) p, int n){//valid(p) && rank(p) + n <= _size
         tail = tail->succ;//待排序区间为(head,tail)
     while (n > 1){//在至少剩两个节点一牵，在待排序区间内
         Posi(T) max = selectMax(head->succ, n);//找到最大元素的位置，歧义时后者优先
-        insertB(tail, remove(max));//将其移至末尾(作为有序区间新的首元素)
+        swap(max->data, tail->data);//交换两者元素，效率高
+        // insertB(tail, remove(max));//将其移至末尾(作为有序区间新的首元素)
         tail = tail->pred; n--;
     }
 }
+// 移动方法效率低，通常为其他基本操作时间的两个数量级及以上
+// 改进措施是交换tail与max指向的数据
+
 // my write selectionSort()
 // while(n > 1){//至少剩下两个节点以前,在待排序区间内
 //     Posi(T) max = selectMax(p, n);//找到最大元素的位置
