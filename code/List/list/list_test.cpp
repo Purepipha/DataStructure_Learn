34c682022bfb465a5242ac0948d070f3a6e0d06c
Swing-code <13299161882@163.com>
2022-02-18 17:40:03 +0800

    fixed the rvalue and lvalue doesn't match

--- code/List/list/list_test.cpp
+++ code/List/list/list_test.cpp
@@ -6,29 +6,20 @@ template <typename T> struct Show{
     virtual void operator()(T const& e) { cout << e << " "; }
 };
 template <typename T> void show(List<T> & L)
-{ L.traverse(Show<T>()); cout << endl; }
+{ Show<T> fshow;L.traverse(fshow); cout << endl; }
 int main(){
     List<int> L;//创建空列表
     for (int i = 0; i < N; i++)//给空列表添加元素
         L.insertAsFirst(rand() % N);
     int r = 0;//统计数目
     clock_t start, end;//统计程序开始结束时间
-    for (Posi(int) p = L.first(); r < N; r++){//从头到尾依次遍历，输出
-        cout << p->data;
-        cout << " ";
-        p = p->succ;
-    }
+    show(L);//调用函数对象
     cout << endl;
     start = clock();
     L.sort(L.first(), N);//使用选择排序
     end = clock();
     cout << "Sort time: " << double(end - start)/CLOCKS_PER_SEC << endl;
-    r = 0;
-    for (Posi(int) p = L.first(); r < N; r++){
-        cout << p->data;
-        cout << " ";
-        p = p->succ;
-    }
+    show(L);
     cout << endl;
     return 0;
 }
