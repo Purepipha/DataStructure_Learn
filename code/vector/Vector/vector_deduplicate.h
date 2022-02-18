7f55da17256added30e10241416635f2e2247b50
Swing-code <13299161882@163.com>
2022-02-18 17:41:29 +0800

    fixed the problem

--- code/vector/Vector/vector_deduplicate.h
+++ code/vector/Vector/vector_deduplicate.h
@@ -1,7 +1,7 @@
 template <typename T>
 int Vector<T>::deduplicate(){//删除无序向量中重复元素(高效版)
     int oldSize = _size;//记录原规模
-    for (Rank i = 1; i <_size)//自前向后逐个考察_elem[1,_size)
+    for (Rank i = 1; i <_size;)//自前向后逐个考察_elem[1,_size)
         if (find(_elem[i], 0, i) < 0)//在前缀[0,i)寻找与之雷同者
             i++;//若无雷同则继续考察其后继
         else
--- code/vector/Vector/vector_selectionSort.h
+++ code/vector/Vector/vector_selectionSort.h
@@ -9,7 +9,7 @@ template <typename T>
 Rank Vector<T>::maxItem(Rank lo, Rank hi){//在[lo,hi]内找出最大值
     Rank mx = hi;
     while (lo < hi--)//逆向扫描
-        if(_elem[hi] > _elem[max])//严格比较
+        if(_elem[hi] > _elem[mx])//严格比较
             mx = hi;//max有多个时保证后者优先，进而保证selectionSort稳定
     return mx;
 }
