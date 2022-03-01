9143117c5f56bbc03ba543ca2326d05496668bb6
Swing-code <13299161882@163.com>
2022-03-01 11:36:37 +0800

    realize stack with list

--- code/Stack/Stack_List.h
+++ code/Stack/Stack_List.h
@@ -2,7 +2,7 @@
 template <typename T>
 class Stack : public List<T>{
 public:
-    void push(T const& e) { insertAsFirst(e); }//入栈
-    T pop() { return remove(first()); }//出栈
-    T & top() { return (*this)[0]; } //取顶
+    void push(T const& e) { insertAsLast(e); }//入栈
+    T pop() { return remove(last()); }//出栈
+    T& top() { return last()->data; } //取栈顶
 };
